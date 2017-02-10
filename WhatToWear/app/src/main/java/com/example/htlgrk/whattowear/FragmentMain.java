1026d4c3efd0f3551efafe41248c7bfa491c62a2
pjedinger <>
2017-02-10 13:45:45 +0100

    FragmentOne got renamed to Fragment Main (Fix)

---
+++ WhatToWear/app/src/main/java/com/example/htlgrk/whattowear/FragmentMain.java
@@ -0,0 +1,49 @@
+package com.example.htlgrk.whattowear;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+
+public class FragmentMain extends Fragment {
+
+    WeatherData wd;
+    String currentCity;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container,
+                false);
+//test
+        if (getArguments() != null) {
+            wd = (WeatherData) getArguments().getSerializable("wd");
+            currentCity = (String) getArguments().getSerializable("location");
+            if (wd != null) {
+
+                TextView loc = (TextView) rootView.findViewById(R.id.location);
+                loc.setText(currentCity);
+                TextView akt = (TextView) rootView.findViewById(R.id.tv_aktTemp);
+
+                akt.setText(wd.currentTemp+"°C");
+                TextView datum = (TextView) rootView.findViewById(R.id.datum);
+                SimpleDateFormat df = new SimpleDateFormat("EEEE, dd.MM.yyyy");
+                datum.setText(df.format(wd.date)); //FORMAT
+                TextView desc = (TextView) rootView.findViewById(R.id.tv_Wetterlage);
+                desc.setText(wd.description);
+                TextView range = (TextView) rootView.findViewById(R.id.tv_tempRange);
+                range.setText(wd.getTempLow() + "°C / " + wd.getTempHigh() + "°C");
+                MyApplication.setClothes(wd.getTempLow(), wd.getTempHigh(), rootView);
+
+            }
+        }
+
+        return rootView;
+    }
+
+}
--- WhatToWear/app/src/main/java/com/example/htlgrk/whattowear/G_Uebersicht.java
+++ WhatToWear/app/src/main/java/com/example/htlgrk/whattowear/G_Uebersicht.java
@@ -4,27 +4,21 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.location.Address;
-import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -33,8 +27,6 @@ import com.example.htlgrk.whattowear.service.YahooWheaterCallback;
 import com.example.htlgrk.whattowear.wheather_data.Channel;
 import com.example.htlgrk.whattowear.wheather_data.Item;
 
-import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -305,7 +297,7 @@ public class G_Uebersicht extends AppCompatActivity implements YahooWheaterCallb
                 //WeatherData wd = new WeatherData(10, 15, 5, "cloudy", 30, new Date(), "c");
                 args.putSerializable("location", currentCity);
                 args.putSerializable("wd", weatherArray[position]);
-                FragmentOne fragment = new FragmentOne();
+                FragmentMain fragment = new FragmentMain();
                 fragment.setArguments(args);
 
                 return fragment;
